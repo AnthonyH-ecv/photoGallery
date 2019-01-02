7b5336d19c7a3c003c49492e3216bbbc3d4c5112
Anthony H <flohoa-a@outlook.com>
2019-01-02 20:08:17 +0100

    filtre js gallery

--- dist/assets/js/modal.js
+++ dist/assets/js/modal.js
@@ -1,34 +1,10 @@
-var typed = new Typed('#typed', {
-    strings: ["Peter Parker", "Photographer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    onComplete: (self) => {showCursor:false},
-    
-});
-
-
-// Smooth Scrolling
-
-$(document).ready(function () {
-    $('.scroll').on('click', function () { // Au clic sur un élément
-        var page = $(this).attr('href'); // Page cible
-        var speed = 1000 // Durée de l'animation (en ms)
-        $('html, body').animate({
-            scrollTop: $(page).offset().top
-        }, speed); // Go
-        return false;
-    });
-});
-
-
-
-
 
 // Get the modal
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.querySelectorAll(".media> img");
+console.log(img);
 var modalImg = document.getElementById("img01");
 
 for (let i = 0; i < img.length; i++) {
@@ -46,4 +22,3 @@ close.onclick = function () {
     modal.style.display = "none";
     console.log("close");
 }
-
