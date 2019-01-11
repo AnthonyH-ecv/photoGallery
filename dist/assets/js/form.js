0432f2efd4ff7b8c17092af00a2d06f1f4122f8e
Anthony H <flohoa-a@outlook.com>
2019-01-11 10:11:52 +0100

    ajout message success and error

--- dist/assets/js/form.js
+++ dist/assets/js/form.js
@@ -1,12 +1,35 @@
 let name = document.getElementById("name");
 let email = document.getElementById("email");
 let message = document.getElementById("message");
+let form = document.getElementsByClassName("form-content");
+let success = document.getElementsByClassName("alert success");
+let error = document.getElementsByClassName("alert error");
+let user = document.getElementById("user");
+let input = [name, email, message];
 
 function submit() {
+  if (name.value && email.value && message.value !== "") {
 
-  if (name.value && email.value && message.value !== '') {
-    console.log("success")
+    user.innerHTML = name.value + " ";
+    success[0].style.display = "block";
+
+    for (let i = 0; i < input.length; i++) {
+
+      input[i].style.borderColor = "rgb(91, 212, 95)";
+      input[i].value = "";
+    }
+
   } else {
-    console.log("error");
+
+    error[0].style.display = "block";
+
+    for (let i = 0; i < input.length; i++) {
+
+      if (input[i].value == "") {
+
+        input[i].style.borderColor = "#f52c1ee3";
+      }
+    }
+
   }
 }
